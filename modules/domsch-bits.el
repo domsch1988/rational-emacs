fbdff98d3c4a6ebc1bc2ceccdc9e6da3eb336913
Dominik Schlack <dominik.schlack@elara-gmbh.de>
2022-02-18 22:11:15 +0100

    First Changes

--- modules/domsch-bits.el
+++ modules/domsch-bits.el
@@ -1,4 +1,5 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(straight-use-package 'spacemacs-theme)
 
 (provide 'domsch-bits)
---
+++ modules/domsch-dashboard.el
@@ -0,0 +1,2 @@
+;;; domsch-dashboard.el ---                          -*- lexical-binding: t; -*-
+(provide 'domsch-dashboard)
